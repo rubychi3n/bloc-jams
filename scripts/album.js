e2f084a0a194f0f55a5fcfd75ee1d7c0c0e09762
Ruby Chien <rubychi3n@gmail.com>
2017-12-22 20:11:16 -0800

    checkpoint 26 work complete

--- scripts/album.js
+++ scripts/album.js
@@ -109,6 +109,41 @@
      }
  };
 
+ var getSongItem = function (element) {
+     switch (element.className) {
+         case 'album-song-button':
+         case 'ion-play':
+         case 'ion-pause':
+             return findParentByClassName(element, 'song-item-number');
+         case 'album-view-song-item':
+             return element.querySelector('.song-item-number');
+         case 'song-item-title':
+         case 'song-item-duration':
+             return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+         case 'song-item-number':
+             return element;
+         default:
+             return;
+     }
+ };
+
+ var clickHandler = function (targetElement) {
+
+     var songItem = getSongItem(targetElement);
+
+     if (currentlyPlayingSong === null) {
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+         songItem.innerHTML = playButtonTemplate;
+         currentlyPlayingSong = null;
+     } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+         var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+         songItem.innerHTML = pauseButtonTemplate;
+         currentlyPlayingSong = songItem.getAttribute('data-song-number');
+     }
+ };
 
  // Creating a parent container that allows us to listen for an event on a parent element
  // but target the behavior of one of its children 
@@ -117,6 +152,12 @@
 
  // Album button templates
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+ // no song is identified as playing until we click one
+ var currentlyPlayingSong = null;
+
+
 
  window.onload = function () {
      setCurrentAlbum(albumPicasso);
@@ -132,7 +173,12 @@
          // use parentElement & className to makesure we only act on the table row
          if (event.target.parentElement.className === 'album-view-song-item') {
              // Change the content from the number to the play button's HTML
-             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+             //  event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+             var songItem = getSongItem(event.target);
+
+             if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                 songItem.innerHTML = playButtonTemplate;
+             }
          }
      });
 
@@ -144,7 +190,37 @@
          songRows[i].addEventListener('mouseleave', function (event) {
              // Selects first child element, which is the song-item-number element
              // getAttribute: takes single argument; a string with the name of the attribute whose value we want to retrieve 
-             this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+             // this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+
+             // #1
+             var songItem = getSongItem(event.target);
+             var songItemNumber = songItem.getAttribute('data-song-number');
+
+             // #2
+
+             // added the conditional that checks that the item the mouse is leaving is not the current song, 
+             // and only change the content if it isn't
+             if (songItemNumber !== currentlyPlayingSong) {
+                 songItem.innerHTML = songItemNumber;
+             }
+
+         });
+
+         songRows[i].addEventListener('click', function (event) {
+             // Event handler call
+             clickHandler(event.target);
          });
      }
  }
+
+
+ // function that keeps traversing the DOM upward until a parent with a specified class name is found
+ var findParentByClassName = function (element, targetClass) {
+     if (element) {
+         var currentParent = element.parentElement;
+         while (currentParent.className !== targetClass && currentParent.className !== null) {
+             currentParent = currentParent.parentElement;
+         }
+         return currentParent;
+     }
+ };
