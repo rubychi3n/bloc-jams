dd182c3767e77f53d5bc544c750d0f4f2fe0f759
Ruby Chien <rubychi3n@gmail.com>
2017-12-28 12:32:08 -0800

    assignment-32-half_way

--- scripts/album.js
+++ scripts/album.js
@@ -263,6 +263,7 @@ var nextSong = function() {
  var currentVolume = 80;
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
+ var $playPauseButton = $('.main-controls .play-pause');
 
 
  var updatePlayerBarSong = function (){
@@ -272,11 +273,26 @@ var nextSong = function() {
     $('.main-controls .play-pause').html(playerBarPauseButton);
  };
 
+ 
+var togglePlayFromPlayerBar = function (){
+    if (currentSoundFile.isPaused()){
+        $().html(pauseButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPauseButton);
+        currentSoundFile.play();
+    } else {
+        $().html(playButtonTemplate);
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+        currentSoundFile.pause();
+    }
+};
+
+
 //  window.onload = function () {
 $(document).ready(function () {
      setCurrentAlbum(albumPicasso);
      $previousButton.click(previousSong);
      $nextButton.click(nextSong);
+     $playPauseButton.click(togglePlayFromPlayerBar);
      
 
     //  songListContainer.addEventListener('mouseover', function (event) {
